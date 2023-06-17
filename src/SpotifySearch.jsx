ff233090a7d88b4f4812a15de3d46f217c96b708
Nicolas Murphy <murphynick10@gmail.com>
2023-06-16 19:46:47 -0500

    works on refresh

--- src/SpotifySearch.jsx
+++ src/SpotifySearch.jsx
@@ -1,14 +1,22 @@
 import React, { useEffect, useState, useRef } from 'react';
-import SpotifyWebApi from 'spotify-web-api-js';
 
 const SpotifySearch = () => {
   const [track, setTrack] = useState(null);
   const [accessToken, setAccessToken] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [isFirstLoad, setIsFirstLoad] = useState(true);
   const audioRef = useRef(null);
 
   useEffect(() => {
     refreshAccessToken();
   }, []);
+
+  useEffect(() => {
+    if (accessToken && isFirstLoad) {
+      setIsFirstLoad(false);
+      getRandomTrack();
+    }
+  }, [accessToken, isFirstLoad]);
 
   const refreshAccessToken = async () => {
     try {
@@ -21,7 +29,7 @@ const SpotifySearch = () => {
         method: 'POST',
         headers: {
           'Content-Type': 'application/x-www-form-urlencoded',
-          'Authorization': `Basic ${encodedCredentials}`,
+          Authorization: `Basic ${encodedCredentials}`,
         },
         body: 'grant_type=client_credentials',
       });
@@ -39,10 +47,7 @@ const SpotifySearch = () => {
 
   const getRandomTrack = async () => {
     try {
-      if (!accessToken) {
-        console.error('Access token not available');
-        return;
-      }
+      setIsLoading(true);
 
       const searchTerm = await fetch('https://random-word-api.vercel.app/api?words=1')
         .then((response) => response.json())
@@ -63,8 +68,11 @@ const SpotifySearch = () => {
       } else {
         console.error('Error retrieving random track:', response.status);
       }
+
+      setIsLoading(false);
     } catch (error) {
       console.error('Error retrieving random track:', error);
+      setIsLoading(false);
     }
   };
 
@@ -77,18 +85,22 @@ const SpotifySearch = () => {
   };
 
   return (
-    <div className='text-center my-12'>
-      <h1 className='my-12'>Spotify Random Track</h1>
-      <button className='btn btn-primary' onClick={getRandomTrack}>Get Random Track</button>
+    <div className="text-center my-12">
+      <h1 className="my-12">Spotify Random Track</h1>
+      <button className="btn btn-primary" onClick={getRandomTrack}>
+        Get Random Track
+      </button>
 
-      {track && (
+      {isLoading && <div>Loading...</div>}
+
+      {!isLoading && track && (
         <div>
           <h2>{track.name}</h2>
           <p>By {track.artists[0].name}</p>
-          <div className='custom-player'>
-            <img src={track.album.images[0].url} alt='Album Art' />
+          <div className="custom-player">
+            <img src={track.album.images[0].url} alt="Album Art" />
             <audio ref={audioRef} controls>
-              <source src={track.preview_url} type='audio/mpeg' />
+              <source src={track.preview_url} type="audio/mpeg" />
               Your browser does not support the audio element.
             </audio>
           </div>
