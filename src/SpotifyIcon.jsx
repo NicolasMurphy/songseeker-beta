658cbdb7b9139eb68073471df4d07f6d3dff245b
Nicolas Murphy <murphynick10@gmail.com>
2023-06-16 21:46:45 -0500

    test deployment

--- src/SpotifyIcon.jsx
+++ src/SpotifyIcon.jsx
@@ -4,7 +4,7 @@ import SpotifyIconImage from "./Images/pngegg.png";
 const SpotifyIcon = () => {
   return (
     <img className="mx-auto m-4 transition duration-300 ease-in-out hover:scale-110"
-        width="96" src={SpotifyIconImage}></img>
+        width="48" src={SpotifyIconImage}></img>
   );
 };
 
