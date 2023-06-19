41aa5534fb5590375e9382677357fbcbe175472e
Nicolas Murphy <murphynick10@gmail.com>
2023-06-19 11:05:14 -0500

    hide track/show track, random song from playlist, etc. working

--- src/SpotifySearch.jsx
+++ src/SpotifySearch.jsx
@@ -6,19 +6,33 @@ const SpotifySearch = () => {
   const [track, setTrack] = useState(null);
   const [accessToken, setAccessToken] = useState("");
   const [isLoading, setIsLoading] = useState(false);
-  const [isFirstLoad, setIsFirstLoad] = useState(true);
+  const [location, setLocation] = useState("");
   const audioRef = useRef(null);
+  const [showTrackInfo, setShowTrackInfo] = useState(false);
 
   useEffect(() => {
-    refreshAccessToken(setAccessToken);
+    const fetchData = async () => {
+      await refreshAccessToken(setAccessToken); // Refresh the access token
+      handleGetRandomTrack();
+    };
+
+    fetchData();
   }, []);
 
-  useEffect(() => {
-    if (accessToken && isFirstLoad) {
-      setIsFirstLoad(false);
-      getRandomTrack(accessToken, setTrack, setIsLoading, resetAudio);
+
+  const handleGetRandomTrack = async () => {
+    setShowTrackInfo(false); // Hide the track info
+    setIsLoading(true);
+
+    try {
+      const token = localStorage.getItem('accessToken');
+      await getRandomTrack(token, setTrack, setIsLoading, resetAudio, setLocation);
+    } catch (error) {
+      console.error('Error retrieving random track:', error);
+      setIsLoading(false);
     }
-  }, [accessToken, isFirstLoad]);
+  };
+
 
   const resetAudio = () => {
     if (audioRef.current) {
@@ -28,15 +42,14 @@ const SpotifySearch = () => {
     }
   };
 
+  const handleShowTrackInfo = () => {
+    setShowTrackInfo(true); // Show the track info
+  };
+
   return (
     <div className="text-center my-12">
       <h1 className="my-12">Spotify Random Track</h1>
-      <button
-        className="btn btn-primary"
-        onClick={() =>
-          getRandomTrack(accessToken, setTrack, setIsLoading, resetAudio)
-        }
-      >
+      <button className="btn btn-primary" onClick={handleGetRandomTrack}>
         Get Random Track
       </button>
 
@@ -44,24 +57,32 @@ const SpotifySearch = () => {
 
       {!isLoading && track && (
         <div>
-          <h2>{track.name}</h2>
-          <p>By {track.artists[0].name}</p>
           <div className="custom-player">
-            <img src={track.album.images[0].url} alt="Album Art" />
             <audio ref={audioRef} controls>
               <source src={track.preview_url} type="audio/mpeg" />
               Your browser does not support the audio element.
             </audio>
           </div>
-          <div className="">
-            <a
-              href={track.external_urls.spotify}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <SpotifyIcon />
-            </a>
-          </div>
+          {!showTrackInfo && (
+            <button className="btn btn-secondary mt-4" onClick={handleShowTrackInfo}>
+              Show Track Info
+            </button>
+          )}
+          {showTrackInfo && (
+            <>
+              <div className="custom-player">
+                <img src={track.album.images[0].url} alt="Album Art" />
+                <h2>{track.name}</h2>
+                <p>By {track.artists[0].name}</p>
+                <p>Location: {location}</p>
+              </div>
+              <div className="spotify-link">
+                <a href={track.external_urls.spotify} target="_blank" rel="noopener noreferrer">
+                  <SpotifyIcon />
+                </a>
+              </div>
+            </>
+          )}
         </div>
       )}
     </div>
--- src/api.jsx
+++ src/api.jsx
@@ -1,60 +1,71 @@
 export const refreshAccessToken = async (setAccessToken) => {
-    try {
-      const clientId = '476c71bb69f4476a8e5b3f4d64c5bb98';
-      const clientSecret = 'fb641cab33dd449cb46f71cdb1ab82d9';
-      const credentials = `${clientId}:${clientSecret}`;
-      const encodedCredentials = btoa(credentials);
+  try {
+    const clientId = process.env.REACT_APP_CLIENT_ID;
+    const clientSecret = process.env.REACT_APP_CLIENT_SECRET;
+    const credentials = `${clientId}:${clientSecret}`;
+    const encodedCredentials = btoa(credentials);
 
-      const response = await fetch('https://accounts.spotify.com/api/token', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
-          Authorization: `Basic ${encodedCredentials}`,
-        },
-        body: 'grant_type=client_credentials',
-      });
+    const response = await fetch('https://accounts.spotify.com/api/token', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        Authorization: `Basic ${encodedCredentials}`,
+      },
+      body: 'grant_type=client_credentials',
+    });
 
-      if (response.ok) {
-        const data = await response.json();
-        setAccessToken(data.access_token);
-      } else {
-        console.error('Error refreshing access token:', response.status);
-      }
-    } catch (error) {
-      console.error('Error refreshing access token:', error);
+    if (response.ok) {
+      const data = await response.json();
+      const accessToken = data.access_token;
+      localStorage.setItem('accessToken', accessToken); // Store the access token in localStorage
+      setAccessToken(accessToken);
+    } else {
+      console.error('Error refreshing access token:', response.status);
     }
-  };
+  } catch (error) {
+    console.error('Error refreshing access token:', error);
+  }
+};
 
-  export const getRandomTrack = async (accessToken, setTrack, setIsLoading, resetAudio) => {
-    try {
-      setIsLoading(true);
 
-      const searchTerm = await fetch('https://random-word-api.vercel.app/api?words=1')
-        .then((response) => response.json())
-        .then((data) => data[0]);
+export const getRandomTrack = async (accessToken, setTrack, setIsLoading, resetAudio, setLocation) => {
+  try {
+    setIsLoading(true);
 
-      const response = await fetch(
-        `https://api.spotify.com/v1/search?q=${encodeURIComponent(searchTerm)}&type=track`,
-        {
-          headers: {
-            Authorization: `Bearer ${accessToken}`,
-          },
-        }
-      );
+    const playlistId = '34fCtmB1IBXo6gZmxAJi2l'; // Replace with the actual playlist ID
 
-      if (response.ok) {
-        const data = await response.json();
-        const randomTrack = data.tracks.items[0];
+    const response = await fetch(`https://api.spotify.com/v1/playlists/${playlistId}/tracks?limit=100`, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
 
-        setTrack(randomTrack);
-        resetAudio();
-      } else {
-        console.error('Error retrieving random track:', response.status);
-      }
+    if (response.ok) {
+      const data = await response.json();
+      const tracks = data.items.map((item) => item.track);
+      const randomIndex = Math.floor(Math.random() * tracks.length);
+      const randomTrack = tracks[randomIndex];
 
-      setIsLoading(false);
-    } catch (error) {
-      console.error('Error retrieving random track:', error);
-      setIsLoading(false);
+      // Define a list of locations matching the playlist index
+      const locations = [
+        "Nigeria",
+        "Romania",
+        // Add more locations as needed
+      ];
+
+      const randomLocationIndex = randomIndex % locations.length;
+      const location = locations[randomLocationIndex];
+
+      setTrack({ ...randomTrack, location }); // Include the location with the track data
+      resetAudio();
+      setLocation(location); // Update the location state
+    } else {
+      console.error('Error retrieving random track:', response.status);
     }
-  };
+
+    setIsLoading(false);
+  } catch (error) {
+    console.error('Error retrieving random track:', error);
+    setIsLoading(false);
+  }
+};
