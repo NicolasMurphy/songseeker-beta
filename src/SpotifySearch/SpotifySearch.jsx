aa6fed2d773e426f609a03fc05808e69fd761ea0
Nicolas Murphy <murphynick10@gmail.com>
2023-10-16 12:49:52 -0500

    styled info color, changed margins

--- src/SpotifySearch/SpotifySearch.jsx
+++ src/SpotifySearch/SpotifySearch.jsx
@@ -253,21 +253,28 @@ const SpotifySearch = ({ database }) => {
               </p>
               {submittingScore ? (
                 <div>
-                  <p>Submit your score to the leaderboard:</p>
+                  <p
+                    className="pt-6"
+                    >
+                    Submit your score to the leaderboard:
+                  </p>
                   <input
+                    className="my-2 input input-bordered input-info w-full max-w-xs"
                     type="text"
                     placeholder="Enter your username"
                     value={username}
                     onChange={(e) => setUsername(e.target.value)}
                   />
+                  <br></br>
                   <button
-                    className="px-4 py-2 bg-info hover:bg-info-focus text-white rounded transition-colors"
+                    className="px-4 py-2 my-2 bg-info hover:bg-[#2c93bf] text-white rounded transition-colors"
                     onClick={handleSubmitScoreToLeaderboard}
                   >
                     Submit Score to Leaderboard
                   </button>
+                  <br></br>
                   <button
-                    className="px-4 py-2 mt-4 bg-primary hover:bg-primary-focus text-white rounded transition-colors"
+                    className="px-4 py-2 my-2 bg-primary hover:bg-primary-focus text-white rounded transition-colors"
                     onClick={handleStartNewGame}
                   >
                     Play Again
@@ -275,7 +282,7 @@ const SpotifySearch = ({ database }) => {
                 </div>
               ) : (
                 <button
-                  className="px-4 py-2 mt-4 bg-primary hover:bg-primary-focus text-white rounded transition-colors"
+                  className="px-4 py-2 my-2 bg-primary hover:bg-primary-focus text-white rounded transition-colors"
                   onClick={handleStartNewGame}
                 >
                   Play Again
