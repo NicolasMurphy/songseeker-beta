197dec1291cf4d2c21034b0b8840abebe0f969b3
Nicolas Murphy <murphynick10@gmail.com>
2023-10-16 12:50:33 -0500

    press enter for clicking buttons

--- src/SpotifySearch/LocationGuess.jsx
+++ src/SpotifySearch/LocationGuess.jsx
@@ -1,17 +1,36 @@
-import React from 'react';
+import React, { useEffect, useRef } from "react";
 
-const LocationGuess = ({ selectedCountry, handleSubmit }) => (
-  <div className="flex justify-center p-6">
-    <button
-      className={`px-4 py-2 text-white rounded ${
-        selectedCountry ? 'bg-accent hover:bg-accent-focus transition-colors' : 'bg-gray-500 opacity-50 cursor-not-allowed'
-      }`}
-      onClick={handleSubmit}
-      disabled={!selectedCountry}
-    >
-      Submit
-    </button>
-  </div>
-);
+const LocationGuess = ({ selectedCountry, handleSubmit }) => {
+  // click enter instead of clicking button
+  const buttonRef = useRef();
+  useEffect(() => {
+    const handleKeyPress = (e) => {
+      if (e.key === "Enter" && buttonRef.current) {
+        buttonRef.current.click();
+      }
+    };
+    document.addEventListener("keypress", handleKeyPress);
+    return () => {
+      document.removeEventListener("keypress", handleKeyPress);
+    };
+  }, []);
+
+  return (
+    <div className="flex justify-center p-6">
+      <button
+        ref={buttonRef}
+        className={`px-4 py-2 text-white rounded ${
+          selectedCountry
+            ? "bg-accent hover:bg-accent-focus transition-colors"
+            : "bg-gray-500 opacity-50 cursor-not-allowed"
+        }`}
+        onClick={handleSubmit}
+        disabled={!selectedCountry}
+      >
+        Submit
+      </button>
+    </div>
+  );
+};
 
 export default LocationGuess;
--- src/SpotifySearch/TrackLoader.jsx
+++ src/SpotifySearch/TrackLoader.jsx
@@ -1,21 +1,37 @@
-import React from 'react';
+import React, { useEffect, useRef } from "react";
 
-const TrackLoader = ({ isLoading, handleGetRandomTrack, handleEndGame, isFinalRound }) => {
+const TrackLoader = ({
+  isLoading,
+  handleGetRandomTrack,
+  handleEndGame,
+  isFinalRound,
+}) => {
   const buttonClass = isFinalRound
-    ? "px-4 py-2 bg-info hover:bg-info-focus text-white rounded transition-colors"
+    ? "px-4 py-2 bg-info hover:bg-[#2c93bf] text-white rounded transition-colors"
     : "px-4 py-2 bg-primary hover:bg-primary-focus text-white rounded transition-colors";
+
+  // click enter instead of clicking button
+  const buttonRef = useRef();
+  useEffect(() => {
+    const handleKeyPress = (e) => {
+      if (e.key === "Enter" && buttonRef.current) {
+        buttonRef.current.click();
+      }
+    };
+    document.addEventListener("keypress", handleKeyPress);
+    return () => {
+      document.removeEventListener("keypress", handleKeyPress);
+    };
+  }, []);
 
   return (
     <button
+      ref={buttonRef}
       className={buttonClass}
       onClick={isFinalRound ? handleEndGame : handleGetRandomTrack}
       disabled={isLoading}
     >
-      {!isFinalRound
-        ? isLoading
-          ? 'Loading...'
-          : 'Next Round'
-        : 'See Score'}
+      {!isFinalRound ? (isLoading ? "Loading..." : "Next Round") : "See Score"}
     </button>
   );
 };
