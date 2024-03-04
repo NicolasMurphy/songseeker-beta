5f7f07fb74d67c04a499274476d0865cc23ddfb1
Nicolas Murphy <murphynick10@gmail.com>
2024-03-04 14:57:53 -0600

    added Türkiye

--- src/utils/DescriptionOptions.js
+++ src/utils/DescriptionOptions.js
@@ -305,6 +305,11 @@ const getDescriptionOptions = () => {
       description: `Sri Lankabhimanya Wannakuwattawaduge Don Albert Perera[4] (Sinhala: වන්නකුවත්තවඩුගේ දොන් ඇල්බට් පෙරේරා; Tamil: டபிள்யூ. டி. அமரதேவா; 5 December 1927 – 3 November 2016), better known by his adopted name Amaradeva, was a prominent Sri Lankan Sinhalese vocalist, violinist and composer. Primarily using traditional instruments like sitars, tablas and harmoniums, he incorporated Sinhala folk music with Indian ragas in his work. Many consider his contribution to the development of Sinhala music as unmatched; hence, he is occasionally cited as the "Maestro of Sri Lankan Music"`,
       link: "https://en.wikipedia.org/wiki/W._D._Amaradeva",
     },
+    {
+      country: "Türkiye",
+      description: `"Kâtibim" ("my clerk"), or "Üsküdar'a Gider İken" ("while going to Üsküdar") is a Turkish folk song about someone's clerk (kâtip) as they travel to Üsküdar. The tune is a famous Istanbul türkü, which is spread beyond Turkey in many countries, especially in the Balkans.`,
+      link: "https://en.wikipedia.org/wiki/K%C3%A2tibim",
+    },
     // {
     //   country: "",
     //   description: ``,
