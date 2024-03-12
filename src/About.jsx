dd63ad1026f9f31388ca3dd03d44a406ea4b9b6e
Nicolas Murphy <murphynick10@gmail.com>
2024-03-11 23:39:13 -0500

    updated about

--- src/About.jsx
+++ src/About.jsx
@@ -6,16 +6,35 @@ const About = () => (
       50 countries!
     </p>
     <h2 className="text-3xl font-bold mb-6">How to Play</h2>
-    <ol className="list-decimal text-lg text-left mx-auto mb-12">
+    <ol className="text-left list-decimal text-lg mb-12 mx-6">
       <li className="mb-4">
         Listen closely to each track — make sure your volume is turned up!
       </li>
       <li className="mb-4">
-        Guess the country of origin on the interactive map.
+        Guess the country of origin on the map.
       </li>
-      <li>
+      <li className="mb-4">
         Collect points for accuracy! The closer your guess, the higher your
         score.
+      </li>
+      <li>
+        Click the{" "}
+        <span className="inline-block text-info">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              className="w-4 h-4 stroke-current"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+              ></path>
+            </svg>
+        </span>{" "}
+        icon to learn more about each track.
       </li>
     </ol>
   </div>
