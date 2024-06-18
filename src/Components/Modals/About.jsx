3ab511fc899ab5d00e447f21a93f1bba565e53c7
Nicolas Murphy <murphynick10@gmail.com>
2024-06-18 18:03:28 -0500

    Refactored List component to use React components instead of raw HTML

--- src/Components/Modals/About.jsx
+++ src/Components/Modals/About.jsx
@@ -4,9 +4,36 @@ const howToPlayItems = [
   "Listen closely to each track — make sure your volume is turned up!",
   "Guess the country of origin on the map.",
   "Collect points for accuracy! The closer your guess, the higher your score.",
-  'Use the <span class="inline-block text-info"><button class="btn btn-circle btn-success pointer-events-none">50/50</button></span> button to narrow down your choices to two countries. You only get one per game, so choose wisely!',
-  'Click the <span class="inline-block text-info"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="w-4 h-4 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg></span> icon to learn more about each track.'
+  <>
+    Use the{" "}
+    <span className="inline-block text-info">
+      <button className="btn btn-circle btn-success pointer-events-none">
+        50/50
+      </button>
+    </span>{" "}
+    button to narrow down your choices to two countries. You only get one per game, so choose wisely!
+  </>,
+  <>
+    Click the{" "}
+    <span className="inline-block text-info">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        className="w-4 h-4 stroke-current"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+        />
+      </svg>
+    </span>{" "}
+    icon to learn more about each track.
+  </>
 ];
+
 
 const About = () => (
   <div className="container mx-auto text-center px-6 py-8">
