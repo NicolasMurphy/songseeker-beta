424e8fdc55993ebd22ccc83fd96ecd9703516f8e
Nicolas Murphy <murphynick10@gmail.com>
2024-06-19 20:11:24 -0500

    helpers to ts

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,10 +1,8 @@
-export const handleGeocoding = async (address) => {
-  //console.log(typeof(address))
+export const handleGeocoding = async (address: string) => {
     const geocoder = new window.google.maps.Geocoder();
     return new Promise((resolve, reject) => {
       geocoder.geocode({ address }, (results, status) => {
-        if (status === 'OK') {
-          //console.log(typeof(results))
+        if (status === 'OK' && results) {
           const { location } = results[0].geometry;
           resolve([location.lat(), location.lng()]);
         } else {
