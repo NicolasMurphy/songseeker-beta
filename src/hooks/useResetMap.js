1089761543e8a939fd9056299f644981dc1eec25
Nicolas Murphy <murphynick10@gmail.com>
2024-06-20 19:24:22 -0500

    useResetMap to ts

--- src/hooks/useResetMap.js
+++
@@ -1,9 +0,0 @@
-import { useEffect } from "react";
-
-export default function useResetMap(shouldResetMap, setSelectedCountry) {
-  useEffect(() => {
-    if (shouldResetMap) {
-      setSelectedCountry("");
-    }
-  }, [shouldResetMap]);
-}
