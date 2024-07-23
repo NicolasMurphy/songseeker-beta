3dab368cc1a74092a8a49350e4e83c40ff945a26
Nicolas Murphy <murphynick10@gmail.com>
2024-07-23 17:50:43 -0500

    trying new hints

--- src/NewVersion/Components/HintsTable.tsx
+++ src/NewVersion/Components/HintsTable.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import useStore from "../store/useStore";
 import useTracks from "../hooks/useTracks";
 import { HintInfoProps } from "../utils/types";
@@ -13,26 +13,28 @@ const censorText = (text: string, words: string[]) => {
   return censoredText;
 };
 
-const HintsTable: React.FC<HintInfoProps> = ({ track, hint }) => {
+const HintsTable: React.FC<HintInfoProps> = ({ track }) => {
   const { tracks } = useTracks();
   const { round, guesses, correctAnswer } = useStore();
+  const [countryData, setCountryData] = useState<any>(null);
 
-  const getBlurAmount = () => {
-    switch (guesses) {
-      case 5:
-        return "blur(10px)";
-      case 4:
-        return "blur(8px)";
-      case 3:
-        return "blur(6px)";
-      case 2:
-        return "blur(4px)";
-      case 1:
-        return "blur(2px)";
-      default:
-        return "none";
+  useEffect(() => {
+    if (correctAnswer) {
+      fetch(`https://restcountries.com/v3.1/name/${correctAnswer}`)
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Failed to fetch country data");
+          }
+          return response.json();
+        })
+        .then((data) => {
+          setCountryData(data[0]);
+        })
+        .catch((error) => {
+          console.error("Error fetching country data:", error);
+        });
     }
-  };
+  }, [correctAnswer, setCountryData]);
 
   const censoredTrackName = censorText(track.name, censoredWords);
   const censoredArtistName = censorText(track.artists[0].name, censoredWords);
@@ -42,28 +44,10 @@ const HintsTable: React.FC<HintInfoProps> = ({ track, hint }) => {
     <>
       {tracks.length !== 0 && round !== null && (
         <div className="card bg-base-300 text-base-content my-4 mx-auto w-full max-w-xs">
-          <h1 className="mt-4 text-xl">Hints</h1>
+          <h1 className="mt-4 text-xl">Hints:</h1>
           <table className="table max-w-xs text-center my-4">
             <tbody>
-              <tr>
-                <td>
-                  Album art:
-                  <img
-                    src={track.album.images[0].url}
-                    alt={`${track.name} album art`}
-                    className="mx-auto m-4 w-48"
-                    style={{ filter: getBlurAmount() }}
-                  />
-                </td>
-              </tr>
               {guesses < 5 && (
-                <tr>
-                  <td>
-                    {track.album.release_date.substring(0,4)}
-                  </td>
-                </tr>
-              )}
-              {guesses < 4 && (
                 <tr>
                   <td>
                     {censoredTrackName} - {censoredArtistName} -{" "}
@@ -71,14 +55,21 @@ const HintsTable: React.FC<HintInfoProps> = ({ track, hint }) => {
                   </td>
                 </tr>
               )}
+              {guesses < 4 && (
+                <tr>
+                  <td>
+                    Languages: {Object.values(countryData.languages).join(", ")}
+                  </td>
+                </tr>
+              )}
               {guesses < 3 && (
                 <tr>
-                  <td>The country starts with "{correctAnswer[0]}".</td>
+                  <td>Population: {countryData.population.toLocaleString()}</td>
                 </tr>
               )}
               {guesses < 2 && (
                 <tr>
-                  <td>Hint: {hint}</td>
+                  <td>The country starts with "{correctAnswer[0]}".</td>
                 </tr>
               )}
             </tbody>
