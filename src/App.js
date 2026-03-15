bf756f5c9d372a0a54ec93266fec3b108360ef11
Nicolas Murphy <murphynick10@gmail.com>
2026-03-14 19:11:49 -0500

    remove landing page and new version

--- src/App.js
+++ src/App.js
@@ -4,10 +4,7 @@ import NotFound from "./NotFound";
 import Footer from "./FooterAndNavComponents/Footer";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import { database, firestore } from "./firebaseConfig";
-import NewVersion from "./NewVersion/Components/NewVersion";
-import { GoogleMapsProvider } from "./NewVersion/context/GoogleMapsContext";
 import { GoogleMapsProviderLegacy } from "./Map/useGoogleMapsLegacy";
-import LandingPage from "./LandingPage";
 
 const LegacyLayout = ({ children }) => (
   <>
@@ -17,36 +14,18 @@ const LegacyLayout = ({ children }) => (
   </>
 );
 
-const NewVersionLayout = ({ children }) => (
-  <>
-    {children}
-    <Footer firestore={firestore} />
-  </>
-);
-
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LandingPage />} />
         <Route
-          path="/legacy"
+          path="/"
           element={
             <GoogleMapsProviderLegacy>
               <LegacyLayout>
                 <CoreLogic database={database} />
               </LegacyLayout>
             </GoogleMapsProviderLegacy>
-          }
-        />
-        <Route
-          path="/new"
-          element={
-            <GoogleMapsProvider>
-              <NewVersionLayout>
-                <NewVersion />
-              </NewVersionLayout>
-            </GoogleMapsProvider>
           }
         />
         <Route path="*" element={<NotFound />} />
