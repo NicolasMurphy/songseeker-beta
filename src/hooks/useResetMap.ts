62c8db9e26c70ffc085cbe2f00b57499e53c4413
Nicolas Murphy <murphynick10@gmail.com>
2026-04-05 17:39:45 -0500

    remove empty folders and unused files

--- src/hooks/useResetMap.ts
+++
@@ -1,9 +0,0 @@
-import { useEffect } from "react";
-
-export default function useResetMap(shouldResetMap: boolean, setSelectedCountry: (country: string) => void) {
-  useEffect(() => {
-    if (shouldResetMap) {
-      setSelectedCountry("");
-    }
-  }, [shouldResetMap, setSelectedCountry]);
-}
